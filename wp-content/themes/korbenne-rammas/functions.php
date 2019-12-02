953d62c815f74c1b3ec9ca64beb51d07512c47a2
17350 <totoriina76380@gmail.com>
2019-12-02 10:09:04 +0100

    debug functions

--- wp-content/themes/korbenne-rammas/functions.php
+++ wp-content/themes/korbenne-rammas/functions.php
@@ -1,6 +1,6 @@
 <?php
 
 
-require get_template_directory() . '/inc/generale.php';
+require get_template_directory() . '/inc/general.php';
 require get_template_directory() . '/inc/extra/template-tags.php';
 require get_template_directory() . '/inc/extra/template-functions.php';
