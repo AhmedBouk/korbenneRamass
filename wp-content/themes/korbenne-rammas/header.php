39753d1bf2f3390890209c2ab9254620e0e11116
gauthierguillaume <gauthier.guillaume@outlook.com>
2019-12-04 09:55:42 +0100

    Add footer.php

--- wp-content/themes/korbenne-rammas/header.php
+++ wp-content/themes/korbenne-rammas/header.php
@@ -16,6 +16,9 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+
 
 	<?php wp_head(); ?>
 </head>
