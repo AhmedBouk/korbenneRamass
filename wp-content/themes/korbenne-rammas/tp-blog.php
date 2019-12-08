695c2bdef0cf330c1d257391a04339585bb5e6e9
ahmed <totoriina76380@gmail.com>
2019-12-08 19:53:57 +0100

    adding single page for blog

--- wp-content/themes/korbenne-rammas/tp-blog.php
+++ wp-content/themes/korbenne-rammas/tp-blog.php
@@ -2,8 +2,8 @@
 
 <?php get_header(); ?>
 
-    <div class="halves-block">
-        <section class="blog half-block">
+    <div class="container">
+        <section class="row">
             <h2 class="title"><span class="title-focus">FROM THE BLOG</span> ALL THE LATEST NEWS</h2>
             <div class="blog-flexslider flexslider">
                 <ul class="slides">
@@ -20,15 +20,14 @@
                     while ($query1->have_posts()) {
                         $query1->the_post();
 
-
                     ?>
                     <li class="blog-slide">
                         <div class="blog-slide-img">
                             <img src="<?= get_the_post_thumbnail(); ?>"
                         </div>
-                        <h3 class="blog-slide-title"><?= get_the_title(); ?></h3>
+                        <h3 class="blog-slide-title" style="background: red; width: 100%"><?= get_the_title(); ?></h3>
                         <p class="blog-slide-info">
-                             <?= get_the_content() ; ?><br>
+                             <?= get_the_excerpt() ; ?><br>
                              <?= get_the_author(); ?>
                         </p>
                         <p class="blog-slide-text"><a href="#" title=""><?= get_the_permalink(); ?></a></p>
